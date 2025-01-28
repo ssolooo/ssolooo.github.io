756bbce370232f0385b3bda0a95d9ba0c51eee7d
ssolooo <solomeistefan@yahoo.com>
2025-01-28 23:22:03 +0000

    Try to solve error with deploy

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738105990',resources: [
+    cacheName: 'chirpy-1738106516',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
