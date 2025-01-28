b925cc2833250c533e84066db2991fd177194fa6
ssolooo <solomeistefan@yahoo.com>
2025-01-28 23:36:55 +0000

    Remove

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738106516',resources: [
+    cacheName: 'chirpy-1738107106',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
